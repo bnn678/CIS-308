42fbe0806357bf6796c8f5bf17d6108a49f1e1bc
Brandon Shaver <bnn678@users.noreply.github.com>
2018-04-11 10:01:43 -0500

    Work on the Exam Review 2

--- ExamReview2/ExamReview2/Source.c
+++ ExamReview2/ExamReview2/Source.c
@@ -1,10 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 // MACROS
 #define MULTIPLY(x,y) ((x)*(y))
+#define BAD(x,y) (x*y)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+#ifdef MYEXAMPLE
+#include 
+#endif
 
 // Conditional Compilation
 
 int main()
 {
+	// MACROS
+	printf("%d %d\n", MULTIPLY(3+1, 4+1), BAD(3+1, 4+1));
+	printf("%d\n", MIN(3, 4));
+
 	// Pointers & Dynamic Memory
 
 	// Linked Lists
@@ -14,4 +27,7 @@ int main()
 	// Function Pointers
 
 	// Memory Allocation Strategies
+
+
+	return 0;
 }
